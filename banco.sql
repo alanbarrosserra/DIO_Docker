b7d76ff552b17a0add558acbd96a7618acab0e4c
alanbarrosserra <alanbarrosserra9@gmail.com>
2023-02-14 08:05:01 -0300

    Create banco.sql

---
+++ banco.sql
@@ -0,0 +1,9 @@
+CREATE TABLE dados (
+    ClienteID int,
+    Nome varchar(50),
+    Sobrenome varchar(50),
+    Endereco varchar(150),
+    Cidade varchar(50),
+    Pontos int,
+    Host varchar(50)
+);
